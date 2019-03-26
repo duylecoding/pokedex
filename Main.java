b781e3238c14411010570def4c4b9fcbbd243afe
Tornado9797 <tornado9797@tamu.edu>
2019-03-25 23:29:41 -0500

    Small Java Template
    Adding a blank "Main" as a template to build off of later.

---
+++ Main.java
@@ -0,0 +1,23 @@
+/*
+
+	Team Members:
+		Conner Joseph Brewster (UIN: 925004339)
+		Duy Le (UIN: 125009667)
+		Adam Morvant (UIN: 924004752)
+	Date: Thursday, April 25th, 2019 at 12:00:00 PM Central Standard Time (Daylight Savings Time)
+	Course: CSCE-470-500 - Information Storage and Retrieval
+	Professor: Ruihong Huang
+	
+	QuickDex - Class Project
+	Information retrieval program for Pokémon species data.
+	
+	Main.java - primary Java file.
+
+*/
+
+public class Main {
+	
+	public static void main (String[] args) {
+		
+	}
+}
